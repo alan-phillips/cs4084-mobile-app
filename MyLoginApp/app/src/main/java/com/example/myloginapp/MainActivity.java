b3b7ad8548173cb8c026c1f629894be52427a28c
andrei-s24 <andrei2002.AS@gmail.com>
2022-04-22 23:15:44 +0100

    Made clickable don't have account text

--- MyLoginApp/app/src/main/java/com/example/myloginapp/MainActivity.java
+++ MyLoginApp/app/src/main/java/com/example/myloginapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myloginapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -10,30 +13,45 @@ import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText username;
+    private EditText password;
+    private Button loginBtn;
+    private TextView toRegister;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        TextView username = findViewById(R.id.username);
-        TextView password = findViewById(R.id.password);
-
-        MaterialButton loginbtn = findViewById(R.id.loginbtn);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginBtn = findViewById(R.id.loginbtn);
+        toRegister = findViewById(R.id.to_register);
 
         //admin and admin
 
-        loginbtn.setOnClickListener(v -> {
-            if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
-                //correct
-                Toast.makeText(MainActivity.this, "Login Successful!!!", Toast.LENGTH_SHORT).show();
-            } else
-                //incorrect
-                Toast.makeText(MainActivity.this, "Login Failed!!!", Toast.LENGTH_SHORT).show();
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    //correct
+                    Toast.makeText(MainActivity.this, "Login Successful!!!", Toast.LENGTH_SHORT).show();
+                } else
+                    //incorrect
+                    Toast.makeText(MainActivity.this, "Login Failed!!!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
-
+        toRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "HOWYA", Toast.LENGTH_SHORT).show();
+            }
         });
 
 
     }
+
 }
