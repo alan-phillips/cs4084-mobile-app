0ad6fbc5654350546317cdf6019cf8bb2d359722
AlteredSanta <Andreu.Kyrychenko@gmail.com>
2022-04-25 02:03:09 +0100

    added home button on the map scene

--- FuelTracker/app/src/main/java/com/example/fueltracker/MapActivity.java
+++ FuelTracker/app/src/main/java/com/example/fueltracker/MapActivity.java
@@ -2,7 +2,11 @@ package com.example.fueltracker;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,13 +17,27 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
     GoogleMap map;
+    private Button goHomebtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        goHomebtn = findViewById(R.id.goHomebtn);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+
+        goHomebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MapActivity.this, "Going to the Home page...", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MapActivity.this, Home.class));
+                finish();
+            }
+        });
     }
 
     @Override
@@ -30,4 +48,5 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         map.addMarker(new MarkerOptions().position(Limerick).title("Limerick"));
         map.moveCamera(CameraUpdateFactory.newLatLng(Limerick));
     }
+
 }
